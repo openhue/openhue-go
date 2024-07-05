220538a766192ecedfd1fec26d6d1c24ab17f35b
Thibault Pensec <39826516+thibauult@users.noreply.github.com>
2024-07-05 12:16:58 +0200

    Adaptations for openhue-cli (#1)

--- helper.go
+++
@@ -1,6 +0,0 @@
-package openhue
-
-func (o *On) Switch() *On {
-	v := !(*o.On)
-	return &On{On: &v}
-}
--- openhue.go
+++ openhue.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"crypto/tls"
 	"errors"
-	sp "github.com/oapi-codegen/oapi-codegen/v2/pkg/securityprovider"
 	"log/slog"
 	"net/http"
 )
@@ -54,10 +53,34 @@ func (h *Home) GetBridgeHome() (*BridgeHomeGet, error) {
 }
 
 //--------------------------------------------------------------------------------------------------------------------//
+// RESOURCE
+//--------------------------------------------------------------------------------------------------------------------//
+
+func (h *Home) GetResources() (map[string]ResourceGet, error) {
+	resp, err := h.api.GetResourcesWithResponse(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.HTTPResponse.StatusCode != http.StatusOK {
+		return nil, newApiError(resp)
+	}
+
+	data := *(*resp.JSON200).Data
+	resources := make(map[string]ResourceGet)
+
+	for _, resource := range data {
+		resources[*resource.Id] = resource
+	}
+
+	return resources, nil
+}
+
+//--------------------------------------------------------------------------------------------------------------------//
 // DEVICE
 //--------------------------------------------------------------------------------------------------------------------//
 
-func (h *Home) GetDevices() ([]DeviceGet, error) {
+func (h *Home) GetDevices() (map[string]DeviceGet, error) {
 	resp, err := h.api.GetDevicesWithResponse(context.Background())
 	if err != nil {
 		return nil, err
@@ -67,14 +90,21 @@ func (h *Home) GetDevices() ([]DeviceGet, error) {
 		return nil, newApiError(resp)
 	}
 
-	return *(*resp.JSON200).Data, nil
+	data := *(*resp.JSON200).Data
+	devices := make(map[string]DeviceGet)
+
+	for _, device := range data {
+		devices[*device.Id] = device
+	}
+
+	return devices, nil
 }
 
 //--------------------------------------------------------------------------------------------------------------------//
 // ROOM
 //--------------------------------------------------------------------------------------------------------------------//
 
-func (h *Home) GetRooms() ([]RoomGet, error) {
+func (h *Home) GetRooms() (map[string]RoomGet, error) {
 	resp, err := h.api.GetRoomsWithResponse(context.Background())
 	if err != nil {
 		return nil, err
@@ -84,14 +114,21 @@ func (h *Home) GetRooms() ([]RoomGet, error) {
 		return nil, newApiError(resp)
 	}
 
-	return *(*resp.JSON200).Data, nil
+	data := *(*resp.JSON200).Data
+	rooms := make(map[string]RoomGet)
+
+	for _, room := range data {
+		rooms[*room.Id] = room
+	}
+
+	return rooms, nil
 }
 
 //--------------------------------------------------------------------------------------------------------------------//
 // LIGHT
 //--------------------------------------------------------------------------------------------------------------------//
 
-func (h *Home) GetLights() ([]LightGet, error) {
+func (h *Home) GetLights() (map[string]LightGet, error) {
 	resp, err := h.api.GetLightsWithResponse(context.Background())
 	if err != nil {
 		return nil, err
@@ -101,14 +138,26 @@ func (h *Home) GetLights() ([]LightGet, error) {
 		return nil, newApiError(resp)
 	}
 
-	return *(*resp.JSON200).Data, nil
+	data := *(*resp.JSON200).Data
+	lights := make(map[string]LightGet)
+
+	for _, light := range data {
+		lights[*light.Id] = light
+	}
+
+	return lights, nil
 }
 
 func (l *LightGet) IsOn() bool {
 	return *l.On.On
 }
 
-func (h *Home) SetLight(lightId string, body UpdateLightJSONRequestBody) error {
+func (l *LightGet) Toggle() *On {
+	v := !(*l.On.On)
+	return &On{On: &v}
+}
+
+func (h *Home) UpdateLight(lightId string, body LightPut) error {
 	_, err := h.api.UpdateLightWithResponse(context.Background(), lightId, body)
 	if err != nil {
 		return err
@@ -120,7 +169,7 @@ func (h *Home) SetLight(lightId string, body UpdateLightJSONRequestBody) error {
 // GROUPED LIGHT
 //--------------------------------------------------------------------------------------------------------------------//
 
-func (h *Home) GetGroupedLights() ([]GroupedLightGet, error) {
+func (h *Home) GetGroupedLights() (map[string]GroupedLightGet, error) {
 	resp, err := h.api.GetGroupedLightsWithResponse(context.Background())
 	if err != nil {
 		return nil, err
@@ -130,14 +179,38 @@ func (h *Home) GetGroupedLights() ([]GroupedLightGet, error) {
 		return nil, newApiError(resp)
 	}
 
-	return *(*resp.JSON200).Data, nil
+	data := *(*resp.JSON200).Data
+	lights := make(map[string]GroupedLightGet)
+
+	for _, light := range data {
+		lights[*light.Id] = light
+	}
+
+	return lights, nil
+}
+
+func (l *GroupedLightGet) IsOn() bool {
+	return *l.On.On
+}
+
+func (l *GroupedLightGet) Toggle() *On {
+	v := !(*l.On.On)
+	return &On{On: &v}
+}
+
+func (h *Home) UpdateGroupedLight(lightId string, body GroupedLightPut) error {
+	_, err := h.api.UpdateGroupedLightWithResponse(context.Background(), lightId, body)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 //--------------------------------------------------------------------------------------------------------------------//
 // SCENE
 //--------------------------------------------------------------------------------------------------------------------//
 
-func (h *Home) GetScenes() ([]SceneGet, error) {
+func (h *Home) GetScenes() (map[string]SceneGet, error) {
 	resp, err := h.api.GetScenesWithResponse(context.Background())
 	if err != nil {
 		return nil, err
@@ -147,23 +220,44 @@ func (h *Home) GetScenes() ([]SceneGet, error) {
 		return nil, newApiError(resp)
 	}
 
-	return *(*resp.JSON200).Data, nil
+	data := *(*resp.JSON200).Data
+	scenes := make(map[string]SceneGet)
+
+	for _, scene := range data {
+		scenes[*scene.Id] = scene
+	}
+
+	return scenes, nil
 }
 
-// NewOpenHueClient Creates a new NewClientWithResponses for a given server and hueApplicationKey.
+func (h *Home) UpdateScene(sceneId string, body ScenePut) error {
+	resp, err := h.api.UpdateSceneWithResponse(context.Background(), sceneId, body)
+	if err != nil {
+		return err
+	}
+
+	if resp.HTTPResponse.StatusCode != http.StatusOK {
+		return newApiError(resp)
+	}
+
+	return nil
+}
+
+//
+// Internal
+//
+
+// newClient creates a new ClientWithResponses for a given Bridge IP and API key.
 // This function will also skip SSL verification, as the Philips HUE Bridge exposes a self-signed certificate.
 func newClient(bridgeIP, apiKey string) (*ClientWithResponses, error) {
-	p, err := sp.NewSecurityProviderApiKey("header", "hue-application-Key", apiKey)
-	if err != nil {
-		return nil, err
+
+	apiKeyAuth := func(ctx context.Context, req *http.Request) error {
+		req.Header.Set("hue-application-key", apiKey)
+		return nil
 	}
+
 	// skip SSL Verification
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 
-	client, err := NewClientWithResponses("https://"+bridgeIP, WithRequestEditorFn(p.Intercept))
-	if err != nil {
-		return nil, err
-	}
-
-	return client, nil
+	return NewClientWithResponses("https://"+bridgeIP, WithRequestEditorFn(apiKeyAuth))
 }
